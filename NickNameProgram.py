591787fb4fb6a802229fe6757cc8e6b676f7fd48
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-05-18 18:33:27 -0500

    Completed_NickNameGeneratorInPython

---
+++ NickNameProgram.py
@@ -0,0 +1,17 @@
+import random, string
+#! python3
+#  After writing in Method save project,
+# Practicing defining methods in Python - 
+# Question 1: how does the return statement work in Python?
+# Question 2: How does splicing strings work in Python?
+
+def guessNickName():
+    name = input("Enter your name, and I will guess your nickname:\n")
+    nickname = name[0:3]
+    print("Your new nickname is " + nickname)
+
+guessNickName()
+
+
+
+
