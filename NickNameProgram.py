59bedb969d67fb890673c921dd25100b9037899e
Des G Bauza <dgb.javadeveloper@gmail.com>
2024-04-11 10:54:59 -0400

    NewChanges

--- NickNameProgram.py
+++ NickNameProgram.py
@@ -1,82 +1,104 @@
 import random, string
-#! python3
-#  After writing in Method save project,
 
 
 # Takes in a NickName 
-def guessNickName():
-    name = input("Enter your name, and I will give you a new nickname: \n")
-    nickname = name[0:3]
-    print("Your new nickname is " + nickname)
-guessNickName()
+def guessNickName(name:str):
+    nickName = name[0:3]
+    return nickName
 
-
-# Takes in a Silly 
-
-def generatePetName():
-    name = input("Enter your name, and I will give you an endearing your nickname:\n")
-    nickname = name[0:3]
-    print("Your new nickname is " + nickname + nickname)
-
-
+# Takes in a Pet Nickname
+def generatePetName(name:str):
+    nickName = name[0:3] + name[0:3]
+    return nickName
 
 # Takes in a NickName and generates a Spanish style name abbreviation
 
-def generateSpanishNickName():
-    name = input("Enter your name, and I will guess your Spanish nickname:\n")
+def generateSpanishNickName(name:str):
     prefix = name[0:3]
-    spanishNickName = nickname + "ito"
-
+    nickName = name + "ito"
     
-    if(prefix.endswith("e"|| "i")) : name[0:4])
+    if(prefix.endswith("e") or prefix.endswith("i")) : 
+        name[0:4]
+        return name +"ito"
     
     else :    
-        spanishNickName = nickname + "ito"
-        spanishNickName2= nickname + "ita"
-    print("Your new Spanish nickname is " +spanishNickName) 
+        return nickName
 
 
-
-# Takes in a NickName and generates an name with alliteration
-def generateAlliterationNickName():
-    name = input("Enter your name, and I will guess your animal inspired nickname:\n")
+# Takes in an Animal nickname
+def generateAnimalNickName(name:str):
     prefix = name[0:3]
    
-    if(prefix[0] == "M") {
-       return "Mambo" + prefix
+    if prefix[0] == "M":
+       nickName = "Tiger" + prefix
+    elif prefix[0]=="A":
+       nickName = "Bunny" + prefix
+    elif prefix[0] == "E":
+        nickName= "Elegant Elephant" + prefix
+    else:
+       nickName= "Sardine" + prefix
+
+    return nickName
+   
+
+
+# Takes in a User's name and returns a DJ name
+def generateDJNickName(name:str):
+    nickname = ""
+    if name.startswith("D") or name.startswith("B") or name.startswith("J") or name.startswith("K"):
+        nickname = "DJ BoogeyDown " + name
+    elif name.startswith("A") or name.startswith("C") or name.startswith("Z") or name.startswith("F"):
+        nickname = "DJ LargerThanLife " + name
+    elif name.startswith("U") or name.startswith("E") or name.startswith("L") or name.startswith("M"):
+        nickname = "DJ Frostbite " +  name
+    elif name.startswith("T") or name.startswith("N") or name.startswith("G") or name.startswith("O"):
+        nickname = "DJ Disco Dancey " + name
+    elif name.startswith("H") or name.startswith("I") or name.startswith("J") or name.startswith("K"):
+        nickname = "DJ Funky " + name
+    elif name.startswith("P") or name.startswith("Q") or name.startswith("R") or name.startswith("S"):
+        nickname = "DJ Boom Boom " + name
+    elif name.startswith("W") or name.startswith("X") or name.startswith("Y") or name.startswith("Z"):
+        nickname = "MC Sonic " + name
+
+    return nickname
+
     
-   } elseif(prefix[0]=="A"){
-       return "Apple" + prefix
-   } elseif(prefix[0] == "E"){
-       return "Elegant Elephant" + prefix
-   } else{
-       "return Saucy Stud" + prefix
-   }
+#Calling the program
+def main():
+    print("Hello, welcome to the Nickname Generator!")
+    name = input("Please enter your name!")
+    print("Hello," + " "+ name + "!")
+
+    while(1==1):
+        text = """
+                Select the number corresponding to the mode you'd like
+                1. General nickname
+                2. Pet nickname 
+                3. Spanish nickname
+                4. Animal nickname
+                5. DJ nickname 
+        """
+        print(text)
+        response = int(input("Please enter a number between 1 and 5 to choose the mode"))
+
+        if response == 1 : 
+            nickName = guessNickName(name)
+        elif response == 2:
+            nickName = generatePetName(name)
+        elif response == 3: 
+            nickName = generateSpanishNickName(name)
+        elif response == 4:
+            nickName = generateAnimalNickName(name)
+        elif response == 5:
+            nickName = generateDJNickName(name)
+        else: 
+            print("Invalid number entered, please choose a number from 1-5.")
+
+        print(nickName)
 
 
-# Takes in a User's name and returns a DJ Nickname Alliteration  
-
-def generateDJNickName():
-    name = input("Enter your name, and I will guess your animal inspired nickname:\n")
-    djBeginnerWords = HashMap;
-    djBeginnerWords.put("B", "BoogeyDown");
-    djBeginnerWords.put("L", "LargerThan");
-    djBeginnerWords.put("S", "ShakinMyBoots");
-    djBeginnerWords.put("U", "UwannaPieceAme")
-    djBeginnerWords.put( "M", "MF")
-    djBeginnerWords.put("T" "LITTY SMITTY")
-    djBeginnerWords.put("D" "DISCOBALL")
-
-
-## TODO Next- determine how to hand out a dj nickname based on first char of input name 
-
-    djBeginnerWords.stream().collect(hashMap.toCollectorList())
-    if (name[0] == djBeginnerWords.getKey()) : return djBeginnerWords.getValue();
-        
-
-
-    
-
+if __name__ == "__main__":
+    main()
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# nicknameGenerator
+
+# Instant Nickname Generator
+A fun and silly nickname generator program for any occasion! The program provides 5 different modes for nickname creation for your name. Need a new StageName? Need a temporary Spanish Name, or need a new nickname idea for your friend group? This app is for you! Simply type in your firstname and ideas made for you, at the click of a button.
+## Motivation
+A project for practicing the Python language, and creating method functionality, and simple String modification operations.
+
+## Features
+This program includes 5 different modes for generating a new nickname, based on your specific preferences!
+
+## Installation
+Runs on Python Language version 3.7.
+
+## How to use?
+To use this program, open on an IDE compatible with Python Language. Then, run the script and enter your First Name without any special characters, to begin the program and generate nicknames!
+
+## Contribute
+To add onto this idea, collaborators can create a new method, which represents another mode of nickname generating, given a user's String input and by applying any strategy of String manipulation! Have fun :) 
+
+MIT © [Des G. Bauza]()
